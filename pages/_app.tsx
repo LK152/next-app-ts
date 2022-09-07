1816bf9f896b3b840224cf440829b9fcfff9c1a9
LK152 <wuluke0@gmail.com>
2022-09-08 03:22:06 +0800

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import * as React from 'react';
+import type { AppProps } from 'next/app';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
+import createEmotionCache from 'utility/createEmotionCache';
+import lightThemeOptions from 'styles/theme/lightThemeOptions';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface MyAppProps extends AppProps {
+    emotionCache?: EmotionCache;
 }
 
-export default MyApp
+const clientSideEmotionCache = createEmotionCache();
+const lightTheme = createTheme(lightThemeOptions);
+
+const MyApp: React.FC<MyAppProps> = (props) => {
+    const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+    return (
+        <CacheProvider value={emotionCache}>
+            <ThemeProvider theme={lightTheme}>
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </CacheProvider>
+    )
+}
+
+export default MyApp;
